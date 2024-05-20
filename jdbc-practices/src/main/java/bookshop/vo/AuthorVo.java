7ac55c3af985e6a03e0a671b638b3cd3360a4ec5
kimjiheej <glp0509@naver.com>
2024-05-20 15:19:33 +0900

    hw_settings

--- jdbc-practices/src/main/java/bookshop/vo/AuthorVo.java
+++ jdbc-practices/src/main/java/bookshop/vo/AuthorVo.java
@@ -18,6 +18,12 @@ public class AuthorVo {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	@Override
+	public String toString() {
+		return "AuthorVo [no=" + no + ", name=" + name + "]";
+	}
 
+	
 
 }
--- jdbc-practices/src/test/java/bookshop/dao/AuthorDaoTest.java
+++ jdbc-practices/src/test/java/bookshop/dao/AuthorDaoTest.java
@@ -1,35 +1,48 @@
 package bookshop.dao;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+import bookshop.vo.AuthorVo;
+
+
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class AuthorDaoTest {
-
-	
-	
 	private static int count = 0;
 	private static AuthorDao authorDao = new AuthorDao();
+	private static AuthorVo mockAuthorVo = new AuthorVo();
 	
 	@BeforeAll
 	public static void setUp() {
-		count = authorDao.finaAll().size();
-		
+		count = authorDao.findAll().size();
+	}
+
+	@Test
+	@Order(1)
+	public void testInsert() {
+		mockAuthorVo.setName("칼세이건");
+		authorDao.insert(mockAuthorVo);
+
+		assertNotNull(mockAuthorVo.getNo());
 	}
 	
-	
 	@Test
-    public void testInsert() {
-        assertTrue(1-1 == 0);
-    }
-	
+	@Order(2)	
+	public void testFindAll() {
+		assertEquals(count + 1, authorDao.findAll().size());
+	}
 	
 	@AfterAll
 	public static void cleanUp() {
-		
+		authorDao.deleteByNo(mockAuthorVo.getNo());
 	}
-	
-
 }
